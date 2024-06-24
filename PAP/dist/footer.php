7eca5d66013fb9f7e685bb0e725d5fd95066a849
ivanvieiraa <ivannvieiraa@outlook.pt>
2024-06-24 12:44:41 +0100

    Abrir redes sociais em novos separadores

--- PAP/dist/footer.php
+++ PAP/dist/footer.php
@@ -1,35 +1,47 @@
 <footer class="border-top py-5 mt-4  ">
-        <div class="container-fluid">
-            <div class="d-flex justify-content-between align-items-center flex-column flex-lg-row">
-                <div>
-                    <ul class="list-unstyled">
-                        <li class="d-inline-block me-1"><a class="text-decoration-none text-dark-hover transition-all"
-                                href="https://www.instagram.com/motospeed.2024/"><i class="ri-instagram-fill"></i></a></li>
-                        <li class="d-inline-block me-1"><a class="text-decoration-none text-dark-hover transition-all"
-                                href="https://github.com/ivanvieiraa/motospeed"><i class="ri-github-fill"></i></a></li>
-                        <li class="d-inline-block me-1"><a class="text-decoration-none text-dark-hover transition-all"
-                                href="https://x.com/MotoSpeed2024"><i class="ri-twitter-fill"></i></a></li>
-                    </ul>
-                </div>
-                <div class="d-flex align-items-center justify-content-end flex-column flex-lg-row">
-                    <p class="small m-0 text-center text-lg-start">&copy; 2024 Motospeed.
-                        
-                    </p>
-                    <ul class="list-unstyled mb-0 ms-lg-4 mt-3 mt-lg-0 d-flex justify-content-end align-items-center">
-                        <li class="bg-light p-2 d-flex align-items-center justify-content-center me-2">
-                            <i class="pi pi-sm pi-paypal"></i>
-                        </li>
-                        <li class="bg-light p-2 d-flex align-items-center justify-content-center me-2">
-                            <i class="pi pi-sm pi-mastercard"></i>
-                        </li>
-                        <li class="bg-light p-2 d-flex align-items-center justify-content-center me-2">
-                            <i class="pi pi-sm pi-american-express"></i>
-                        </li>
-                        <li class="bg-light p-2 d-flex align-items-center justify-content-center"><i
-                                class="pi pi-sm pi-visa"></i>
-                        </li>
-                    </ul>
-                </div>
+    <div class="container-fluid">
+        <div class="d-flex justify-content-between align-items-center flex-column flex-lg-row">
+            <div>
+                <ul class="list-unstyled">
+                    <li class="d-inline-block me-1">
+                        <a class="text-decoration-none text-dark-hover transition-all"
+                            href="https://www.instagram.com/motospeed.2024/" target="_blank">
+                            <i class="ri-instagram-fill"></i>
+                        </a>
+                    </li>
+                    <li class="d-inline-block me-1">
+                        <a class="text-decoration-none text-dark-hover transition-all"
+                            href="https://github.com/ivanvieiraa/motospeed" target="_blank">
+                            <i class="ri-github-fill"></i>
+                        </a>
+                    </li>
+                    <li class="d-inline-block me-1">
+                        <a class="text-decoration-none text-dark-hover transition-all"
+                            href="https://x.com/MotoSpeed2024" target="_blank">
+                            <i class="ri-twitter-fill"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="d-flex align-items-center justify-content-end flex-column flex-lg-row">
+                <p class="small m-0 text-center text-lg-start">&copy; 2024 Motospeed.
+
+                </p>
+                <ul class="list-unstyled mb-0 ms-lg-4 mt-3 mt-lg-0 d-flex justify-content-end align-items-center">
+                    <li class="bg-light p-2 d-flex align-items-center justify-content-center me-2">
+                        <i class="pi pi-sm pi-paypal"></i>
+                    </li>
+                    <li class="bg-light p-2 d-flex align-items-center justify-content-center me-2">
+                        <i class="pi pi-sm pi-mastercard"></i>
+                    </li>
+                    <li class="bg-light p-2 d-flex align-items-center justify-content-center me-2">
+                        <i class="pi pi-sm pi-american-express"></i>
+                    </li>
+                    <li class="bg-light p-2 d-flex align-items-center justify-content-center"><i
+                            class="pi pi-sm pi-visa"></i>
+                    </li>
+                </ul>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>
